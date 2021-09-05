c1470e84209c45fcedb4935586f0e78922f2598b
Eli R <einthesky@outlook.com>
2021-09-04 21:55:54 -0700

    Updates to schema.sql and connection.js files

--- connection.js
+++ connection.js
@@ -5,10 +5,34 @@ const inquirer = require ('inquirer');
 const connection = mysql.createConnection({
     host: "localhost",
 
+    port: 3306,
+
     user: "root",
     
     password: "c1h2u3c4k5",
     
     database: "employees_db"
 });
+
+connection.connect((err) => {
+   if (err) throw err;
+   listSearch(); 
+});
+
+const listSearch = () => {
+    inquirer
+        .prompt({
+            name: 'departmentName',
+            type: 'list',
+            message: 'Pick a company department:',
+        })
+        .then((answer) => {
+            connection.query('SELECT * FROM department', (err, res) => {
+                if (err) throw err;
+                // Log all results of the SELECT statement
+                console.log(res);
+                connection.end();
+              });
+        });
+};
 
