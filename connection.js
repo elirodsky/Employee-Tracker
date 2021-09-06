608609d55307ccf8b4a81d78d0dae837faeba010
Eli R <einthesky@outlook.com>
2021-09-06 12:24:37 -0700

    Updated Connection.JS an node_modules to submit to local repository

--- connection.js
+++ connection.js
@@ -3,36 +3,113 @@ const inquirer = require ('inquirer');
 
 //establishing connection to sql databse
 const connection = mysql.createConnection({
-    host: "localhost",
-
+    host: 'localhost',
     port: 3306,
-
-    user: "root",
-    
-    password: "c1h2u3c4k5",
-    
-    database: "employees_db"
+    user: 'root',
+    password: 'c1h2u3c4k5',
+    database: 'employees_db'
 });
 
 connection.connect((err) => {
-   if (err) throw err;
-   listSearch(); 
-});
+  if (err) throw err;
+  runSearchDirectory();
+  });
 
-const listSearch = () => {
+const runSearchDirectory = () => {
     inquirer
-        .prompt({
-            name: 'departmentName',
-            type: 'list',
-            message: 'Pick a company department:',
-        })
-        .then((answer) => {
-            connection.query('SELECT * FROM department', (err, res) => {
-                if (err) throw err;
-                // Log all results of the SELECT statement
-                console.log(res);
-                connection.end();
-              });
-        });
-};
+      .prompt({
+        name: 'action',
+        type: 'list',
+        message: 'View Company Directory:',
+        choices: [
+          'Find a department:',
+          'Find employee positions:',
+          'Find employee names:',
+          'Exit',
+        ],
+    })   
+    .then((answer) => {
+        switch (answer.action) {
+          case 'Find a department:':
+            departmentSearch();
+            break;
 
+          case 'Find employee positions':
+            roleSearch();
+            break;
+
+          case 'Find employee names:':
+            employeeSearch();
+            break;
+
+          case 'Exit':
+            connection.end();
+            break;
+
+          default:
+            console.log(`Invalid action: ${answer.action}`);
+            break;
+        }
+    });
+  };
+  
+  const departmentSearch = () => {
+    inquirer
+      .prompt({
+        name: 'departmentName',
+        type: 'input',
+        message: 'Find a department:',
+      })
+      .then((answer) => { 
+        const query = 'SELECT id, departmentName FROM department WHERE ?'; 
+        connection.query(query, { departmentName: answer.departmentName }, (err, res) => {
+        if (err) throw err;
+        res.forEach(({ id, departmentName }) => {
+          console.log(
+            `ID: ${id} || Department: ${departmentName}`
+            );
+          });  
+        runSearchDirectory();
+      });
+    });
+  };
+
+  const roleSearch = () => {
+    inquirer
+      .prompt({
+        name: 'title',
+        type: 'input',
+        message: 'Find employee positions:',
+    }) 
+    .then((answer) => { 
+      const query = 'SELECT id, title, salary, department_id FROM employees WHERE ?';
+      connection.query(query, { title: answer.title }, (err, res) => {
+        if (err) throw err;
+      res.forEach(({ id, title, salary, department_id }) => {
+          console.log(`ID: ${id} || Title: ${title} || Salary: ${salary} || Department ID: ${department_id}`
+          );
+        });
+        runSearchDirectory();
+      });
+    });
+  };  
+
+  const employeeSearch = () => {
+    inquirer
+      .prompt({
+        name: 'last_name',
+        type: 'input',
+        message: 'Find employee names:',
+    })   
+    .then((answer) => {
+      const query = 'SELECT id, first_name, last_name, role_id, manager_id FROM employees WHERE ?';
+      connection.query(query, { last_name: answer.last_name }, (err, res) => {
+        if (err) throw err;
+      res.forEach(({ id, first_name, last_name, role_id, manager_id }) => {
+          console.log(`ID: ${id} | First Name: ${first_name} | Last Name: ${last_name} | Role ID: ${role_id} | Manager ID: ${manager_id}`
+        );
+        });
+        runSearchDirectory();   
+      });
+  });
+};
