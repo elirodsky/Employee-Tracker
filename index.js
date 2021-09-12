5e07ea2aeff2d94c59fa6f3012ae6e24c5469b1d
Eli R <einthesky@outlook.com>
2021-09-12 14:28:26 -0700

    restructing files by renaming and updating

--- index.js
+++ index.js
@@ -1,133 +1,56 @@
-const { prompt } = require('inquirer');
-const db = require('./db');
-require('console.table');
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+require('dotenv').config();
 
-async function loadMainPrompts(){
-    const { choice } = await prompt([
-        {
-            type: "list",
-            name: "choice",
-            message: "What would you like to do?",
-            choices: [
-        {
-          name: "View Employees",
-          value: "VIEW_EMPLOYEES"
-        },
-        {
-          name: "View Employees By Department",
-          value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
-        },
-        {
-          name: "View Employees By Manager",
-          value: "VIEW_EMPLOYEES_BY_MANAGER"
-        },
-        {
-          name: "View All Roles",
-          value: "VIEW_ROLES"
-        },
-        {
-          name: "View All Departments",
-          value: "VIEW_DEPARTMENTS"
-        },
-        {
-          name: "Quit",
-          value: "QUIT"
-        }
-      ]
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}\n`);
+  //Add start function
+  runSearchDirectory();
+})
+
+const runSearchDirectory = () => {
+  inquirer.prompt({
+    name: 'main',
+    type: 'list',
+    message: 'What do you want to do?',
+    choices: [
+      'Add another department',
+      'Add another role',
+      'Add another employee',
+      'Exit',
+    ],
+  })
+  .then((answer) => {
+    switch (answer.main){
+      case 'Add a department':
+        departmentAdd();
+        break;
+
+      case 'Add a role':
+        roleAdd();
+        break;
+
+      case 'Add an employee':
+        employeeAdd();
+        break;
+
+      case 'Exit':
+        connection.end;
+        console.log('Thank you!');
+        break;
+
+      default:
+        console.log(`Invalid action: ${answer.action}`);
+        break;
     }
-  ]);
-
-  switch (choice) {
-    case "VIEW_EMPLOYEES":
-      return viewEmployees();
-    case "VIEW_EMPLOYEES_BY_DEPARTMENT":
-      return viewEmployeesByDepartment();
-    case "VIEW_EMPLOYEES_BY_MANAGER":
-      return viewEmployeesByManager();
-    case "VIEW_DEPARTMENTS":
-      return viewDepartments();
-    case "VIEW_ROLES":
-      return viewRoles();
-    default:
-      return quit();
-  }
+  })
 }
-
-async function viewEmployees() {
-  const employees = await db.findAllEmployees();
-
-  console.log("\n");
-  console.table(employees);
-
-  loadMainPrompts();
-}
-
-async function viewEmployeesByDepartment() {
-  const departments = await db.findAllDepartments();
-
-  const departmentChoices = departments.map(({ id, name }) => ({
-    name: name,
-    value: id
-  }));
-
-  const { departmentId } = await prompt([
-    {
-      type: "list",
-      name: "departmentId",
-      message: " In what department are you seeking employees?",
-      choices: departmentChoices
-    }
-  ]);
-
-  const employees = await db.findAllEmployeesByDepartment(departmentId);
-
-  console.log("\n");
-  console.table(employees);
-
-  loadMainPrompts();
-}
-
-async function viewEmployeesByManager() {
-  const managers = await db.findAllEmployees();
-
-  const managerChoices = managers.map(({ id, first_name, last_name }) => ({
-    name: `${first_name} ${last_name}`,
-    value: id
-  }));
-
-  const { managerId } = await prompt([
-    {
-      type: "list",
-      name: "managerId",
-      message: "Which employee employee reports do you want to see?",
-      choices: managerChoices
-    }
-  ]);
-
-  const employees = await db.findAllEmployeesByManager(managerId);
-
-  console.log("\n");
-
-  if (employees.length === 0) {
-    console.log("The selected employee has no direct reports");
-  } else {
-    console.table(employees);
-  }
-
-  loadMainPrompts();
-}
-
-  async function viewRoles() {
-  const roles = await db.findAllRoles();
-
-  console.log("\n");
-  console.table(roles);
-
-  loadMainPrompts();
-}
-
-function quit() {
-  console.log("Thank you!");
-  process.exit();
-}
-
